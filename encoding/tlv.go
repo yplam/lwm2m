6e9f8a7d8ff543857b61aaf0ac0e3a285e819d58
yplam <yplam@yplam.com>
2021-03-19 16:02:13 +0800

    add simple tlv encoding support

---
+++ encoding/tlv.go
@@ -0,0 +1,283 @@
+package encoding
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"math"
+)
+
+var (
+	ErrTLVInvalidLength = errors.New("invalid core link string value")
+	ErrTLVNotEnoughData = errors.New("not enough data")
+)
+
+type TLVType byte
+
+var (
+	ObjectInstance       TLVType = 0 // Object Instance in which case the Value contains one or more Resource TLVs
+	MultipleResourceItem TLVType = 1 // Resource Instance with Value for use	within a multiple Resource TLV
+	MultipleResource     TLVType = 2 // multiple Resource, in which case the	Value contains one or more Resource Instance TLVs
+	SingleResource       TLVType = 3 // Resource with Value
+)
+
+// TLV is Type-Length-Value encoding format for lwm2m
+//
+// -------------------------------------------------------------------------------
+// + Type       + 8 bits    + Bit 7-6: Indicates the type of Identifier.
+// +            +           + 00 = Object Instance in which case the Value
+// +            +           +      contains one or more Resource TLVs
+// +            +           + 01 = Resource Instance with Value for use
+// +            +           +      within a multiple Resource TLV
+// +            +           + 10 = multiple Resource, in which case the Value
+// +            +           +       contains one or more Resource Instance TLVs
+// +            +           + 11 = Resource with Value
+// +            +           +------------------------------------------------------
+// +            +           + Bit 5: Indicates the Length of the Identifier.
+// +            +           + 0 = The Identifier field of this TLV is 8 bits long
+// +            +           + 1 =The Identifier field of this TLV is 16 bits long
+// +            +           +------------------------------------------------------
+// +            +           + Bit 4-3: Indicates the type of Length.
+// +            +           + 00 = No length field, the value immediately
+// +            +           +      follows the Identifier field in is of the length
+// +            +           +      indicated by Bits 2-0 of this field
+// +            +           + 01 = The Length field is 8-bits and Bits 2-0 MUST be ignored
+// +            +           + 10 = The Length field is 16-bits and Bits 2-0 MUST be ignored
+// +            +           + 11 = The Length field is 24-bits and Bits 2-0 MUST be ignored
+// +            +           +------------------------------------------------------
+// +            +           + Bits 2-0: A 3-bit unsigned integer indicating
+// +            +           + the Length of the Value.
+// +------------+-----------+------------------------------------------------------
+// + Identifier + 8/16 bits + The Object Instance, Resource, or Resource
+// +            +           + Instance ID as indicated by the Type field.
+// +------------+-----------+------------------------------------------------------
+// + Length     + 0-24 bits + The Length of the following field in bytes.
+// +------------+-----------+------------------------------------------------------
+// + Value      + bytes     + Value of the tag.
+// -----------------------------------------------------------------------------
+type TLV struct {
+	Type       TLVType
+	Identifier uint16
+	Value      []byte
+	Length     uint32
+	Children   []*TLV
+}
+
+func (t *TLV) Unmarshal(data []byte) (uint32, error) {
+	dlen := uint32(len(data))
+	if dlen == 0 {
+		return 0, nil
+	}
+	if dlen < 2 {
+		return 0, ErrTLVNotEnoughData
+	}
+	//log.Printf("%#v", data)
+	t.Type = TLVType((data[0] >> 6) & 0x03)
+
+	var offset uint32 = 1
+	idLen := uint32((data[0]>>5)&0x01 + 1)
+	if dlen < offset+idLen {
+		return 0, ErrTLVNotEnoughData
+	}
+	t.Identifier = uint16(tlvLen(data[offset : offset+idLen]))
+
+	offset = offset + idLen
+	lenType := uint32((data[0] >> 3) & 0x03)
+	if dlen < offset+lenType {
+		return 0, ErrTLVNotEnoughData
+	}
+	if lenType == 0 {
+		t.Length = uint32(data[0] & 0x07)
+	} else {
+		t.Length = tlvLen(data[offset : offset+lenType])
+		offset = lenType + offset
+	}
+
+	if dlen < offset+t.Length {
+		return 0, ErrTLVNotEnoughData
+	}
+	t.Value = data[offset : offset+t.Length]
+	offset = offset + t.Length
+	if t.Type == ObjectInstance || t.Type == MultipleResource {
+		//log.Printf("decode children %v, %#v", t.Length, t.Value)
+		c, err := DecodeTLVs(t.Value)
+		if err != nil {
+			return 0, err
+		}
+		t.Children = c
+	}
+	//log.Printf("total : %v, %#v", offset, t)
+	return offset, nil
+}
+
+// @todo need more tests
+func (t *TLV) Marshal() []byte {
+	if len(t.Children) > 0 {
+		t.Value = EncodeTLVs(t.Children)
+		t.Length = uint32(len(t.Value))
+	}
+	data := make([]byte, 0, t.Length+6)
+	// Identifier is always two bytes
+	var dType byte = 0x20
+	dType |= byte(t.Type << 6)
+	//log.Printf("%#v", t.Length)
+	if t.Length <= 7 {
+		dType |= byte(t.Length)
+		data = append(data, dType, byte((t.Identifier>>8)&0xFF), byte(t.Identifier&0xFF))
+	} else if t.Length <= 0xFF {
+		dType |= byte(0x01 << 3)
+		data = append(data, dType, byte((t.Identifier>>8)&0xFF), byte(t.Identifier&0xFF),
+			byte(t.Length))
+	} else if t.Length <= 0xFFFF {
+		dType |= byte(0x10 << 3)
+		data = append(data, dType, byte((t.Identifier>>8)&0xFF), byte(t.Identifier&0xFF),
+			byte((t.Length&0xFF00)>>8), byte(t.Length&0xFF))
+	} else if t.Length <= 0xFFFFFF {
+		dType |= byte(0x11 << 3)
+		data = append(data, dType, byte((t.Identifier>>8)&0xFF), byte(t.Identifier&0xFF),
+			byte((t.Length&0xFF0000)>>16), byte((t.Length&0xFF00)>>8), byte(t.Length&0xFF))
+	}
+	data = append(data, t.Value...)
+	return data
+}
+
+// return the total size of the encoded bytes
+func (t *TLV) marshalCap() (c uint32) {
+	c = 3
+	var l uint32
+	if len(t.Children) > 0 {
+		for _, cc := range t.Children {
+			c += cc.marshalCap()
+		}
+		l = c - 3
+	} else {
+		l = t.Length
+	}
+	if l <= 0xFF {
+		c += 1
+	} else if l <= 0xFFFF {
+		c += 2
+	} else if l <= 0xFFFFFF {
+		c += 3
+	}
+	return
+}
+
+// UTF-8 string
+func (t *TLV) String() (string, error) {
+	return string(t.Value), nil
+}
+
+func (t *TLV) Integer() (val int64, err error) {
+	buff := bytes.NewBuffer(t.Value)
+	if t.Length == 1 {
+		var i1 int8
+		err = binary.Read(buff, binary.BigEndian, &i1)
+		val = int64(i1)
+	} else if t.Length == 2 {
+		var i2 int16
+		err = binary.Read(buff, binary.BigEndian, &i2)
+		val = int64(i2)
+	} else if t.Length == 4 {
+		var i4 int32
+		err = binary.Read(buff, binary.BigEndian, &i4)
+		val = int64(i4)
+	} else if t.Length == 8 {
+		var i8 int64
+		err = binary.Read(buff, binary.BigEndian, &i8)
+		val = i8
+	} else {
+		err = ErrTLVInvalidLength
+	}
+	return
+}
+
+func (t *TLV) Float() (float64, error) {
+	if t.Length == 4 {
+		return float64(math.Float32frombits(binary.BigEndian.Uint32(t.Value))), nil
+	} else if t.Length == 8 {
+		return math.Float64frombits(binary.BigEndian.Uint64(t.Value)), nil
+	} else {
+		return 0, ErrTLVInvalidLength
+	}
+}
+
+func (t *TLV) Boolean() (val bool, err error) {
+	if t.Length != 1 {
+		return false, ErrTLVInvalidLength
+	}
+	val = t.Value[0] != 0
+	return
+}
+
+func (t *TLV) Opaque() ([]byte, error) {
+	return t.Value, nil
+}
+
+func (t *TLV) Time() (int64, error) {
+	if t.Length == 4 {
+		return int64(binary.BigEndian.Uint32(t.Value)), nil
+	} else if t.Length == 8 {
+		return int64(binary.BigEndian.Uint64(t.Value)), nil
+	} else {
+		return 0, ErrTLVInvalidLength
+	}
+}
+
+func (t *TLV) ObjectLink() (uint16, uint16, error) {
+	if t.Length != 4 {
+		return 0, 0, ErrTLVInvalidLength
+	}
+	_ = t.Value[3]
+	return uint16(t.Value[0])*256 + uint16(t.Value[1]),
+		uint16(t.Value[2])*256 + uint16(t.Value[3]), nil
+}
+
+func tlvLen(d []byte) (l uint32) {
+	for _, v := range d {
+		l = l<<8 + uint32(v)
+	}
+	return
+}
+
+func DecodeTLVs(data []byte) ([]*TLV, error) {
+	var tlvs []*TLV
+	var offset uint32 = 0
+	var err error
+	//log.Printf("%#v", data)
+	for {
+		t := &TLV{}
+		o, err := t.Unmarshal(data[offset:])
+		offset += o
+		if err != nil {
+			return nil, err
+		}
+		if o == 0 {
+			break
+		}
+		tlvs = append(tlvs, t)
+	}
+	return tlvs, err
+}
+
+func EncodeTLVs(tlvs []*TLV) []byte {
+	var c uint32 = 0
+	for _, tlv := range tlvs {
+		c += tlv.marshalCap()
+	}
+	data := make([]byte, 0, c)
+	for _, tlv := range tlvs {
+		data = append(data, tlv.Marshal()...)
+	}
+	return data
+}
+
+func NewTLV(t TLVType, id uint16, v []byte) *TLV {
+	return &TLV{
+		Type:       t,
+		Identifier: id,
+		Value:      v,
+		Length:     uint32(len(v)),
+		Children:   make([]*TLV, 0),
+	}
+}
--- encoding/tlv_test.go
+++ encoding/tlv_test.go
@@ -1,13 +1,14 @@
-package lwm2m
+package encoding
 
 import (
 	"encoding/hex"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"strings"
 	"testing"
 )
 
-func TestTLVLen(t *testing.T)  {
+func TestTLVLen(t *testing.T) {
 	assert.Equal(t, tlvLen([]byte{0x03}), uint32(3))
 	assert.Equal(t, tlvLen([]byte{0x03, 0x34}), uint32(0x0334))
 }
@@ -45,7 +46,7 @@ func TestSingleObjectTLV(t *testing.T) {
 	C1 10 55`
 	data, err := strToByte(str)
 	assert.Nil(t, err)
-	//log.Printf("%#v", data)
+	log.Printf("%#v", data)
 	v, err := DecodeTLVs(data)
 	assert.Nil(t, err)
 	//log.Printf("%#v", v)
@@ -55,34 +56,33 @@ func TestSingleObjectTLV(t *testing.T) {
 	assert.Equal(t, len(v[6].Children), 2)
 	assert.Equal(t, len(v[9].Children), 1)
 
-	s, _ := v[0].StringVal()
+	s, _ := v[0].String()
 	assert.Equal(t, s, "Open Mobile Alliance")
-	s, _ = v[1].StringVal()
+	s, _ = v[1].String()
 	assert.Equal(t, s, "Lightweight M2M Client")
 
-	i8, err := v[7].Int8Val()
+	i8, err := v[7].Integer()
 	assert.Nil(t, err)
-	assert.Equal(t, i8, int8(0x64))
-	u8, err := v[7].Uint8Val()
+	assert.Equal(t, i8, int64(0x64))
+	u8, err := v[7].Integer()
 	assert.Nil(t, err)
-	assert.Equal(t, u8, uint8(0x64))
+	assert.Equal(t, u8, int64(0x64))
 
-	i16, err := v[5].Children[0].Int16Val()
+	i16, err := v[5].Children[0].Integer()
 	assert.Nil(t, err)
-	assert.Equal(t, i16, int16(0x0ed8))
-	u16, err := v[5].Children[1].Uint16Val()
+	assert.Equal(t, i16, int64(0x0ed8))
+	u16, err := v[5].Children[1].Integer()
 	assert.Nil(t, err)
-	assert.Equal(t, u16, uint16(0x1388))
+	assert.Equal(t, u16, int64(0x1388))
 
-	u32, err := v[10].Uint32Val()
+	u32, err := v[10].Integer()
 	assert.Nil(t, err)
-	assert.Equal(t, u32, uint32(0x5182428F))
-	i32, err := v[10].Int32Val()
+	assert.Equal(t, u32, int64(0x5182428F))
+	i32, err := v[10].Integer()
 	assert.Nil(t, err)
-	assert.Equal(t, i32, int32(0x5182428F))
+	assert.Equal(t, i32, int64(0x5182428F))
 
 }
-
 
 func TestSingleInstanceObjectTLV(t *testing.T) {
 	str := `
@@ -116,8 +116,6 @@ C1 10 55
 	assert.Equal(t, len(v), 1)
 	assert.Equal(t, len(v[0].Children), 13)
 }
-
-
 
 func TestMultipleInstanceObjectTLV(t *testing.T) {
 	str := `
@@ -187,4 +185,12 @@ C4 02 FF FF FF FF
 	_, err = DecodeTLVs(data)
 	assert.Nil(t, err)
 
+}
+
+func TestSimpleEncodeDecode(t *testing.T) {
+	data := []byte{0xe1, 0x15, 0x7c, 0x0}
+	tlvs, err := DecodeTLVs(data)
+	assert.Nil(t, err)
+	data2 := EncodeTLVs(tlvs)
+	log.Printf("%#v", data2)
 }
