9cc793f32da0bc8640ac7c84905ddf8dd572744e
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2023-06-13 15:07:22 +0300

    fix handleDelete: nil device dereference

--- registration/handler.go
+++ registration/handler.go
@@ -133,11 +133,11 @@ func (h *Handler) handleUpdate(w mux.ResponseWriter, r *mux.Message, id string) 
 
 func (h *Handler) handleDelete(w mux.ResponseWriter, r *mux.Message, id string) {
 	d, err := h.manager.GetDevice(id)
-	if err != nil {
+	if err != nil || d == nil {
 		_ = w.SetResponse(codes.NotFound, message.TextPlain, nil)
-	} else {
-		_ = w.SetResponse(codes.Deleted, message.TextPlain, nil)
+		return
 	}
+	_ = w.SetResponse(codes.Deleted, message.TextPlain, nil)
 	_ = h.manager.Deregister(d.Id)
 }
 
